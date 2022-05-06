d0d6c1737f3a1aa2cf87d07517c19d8b5daa1d34
waffe_x20 <argueta_eduardo@yahoo.de>
2022-05-05 19:12:08 -0600

    adds jsonify (aus 26.3)

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hallo Welt!'
+    return jsonify(message='Hallo Welt!!'), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='not found'), 404
+
+
+@app.route("/parameters")
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message='leider ' + name), 401
+    else:
+        return jsonify(message='Willkommen ' + name)
+
+
+@app.route('/url_vars/<string:name>/<int:age>')
+def url_vars(name: str, age: int):
+    if age < 18:
+        return jsonify(message='leider ' + name), 401
+    else:
+        return jsonify(message='Willkommen ' + name)
 
 
 if __name__ == '__main__':
