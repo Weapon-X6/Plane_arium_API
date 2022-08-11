047ff8126732d0cf14a4eeb3470368c0e243b85a
waffe_x20 <argueta_eduardo@yahoo.de>
2022-08-11 16:13:25 -0600

    Adds planets endpoint

--- app.py
+++ app.py
@@ -1,62 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
-
 import os
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
 
 db = SQLAlchemy(app)
-
-
-@app.route('/')
-def hello_world():
-    return jsonify(message='Hallo Welt!!'), 200
-
-
-@app.route('/not_found')
-def not_found():
-    return jsonify(message='not found'), 404
-
-
-@app.route("/parameters")
-def parameters():
-    name = request.args.get('name')
-    age = int(request.args.get('age'))
-    if age < 18:
-        return jsonify(message='leider ' + name), 401
-    else:
-        return jsonify(message='Willkommen ' + name)
-
-
-@app.route('/url_vars/<string:name>/<int:age>')
-def url_variables(name: str, age: int):
-    if age < 18:
-        return jsonify(message='leider ' + name), 401
-    else:
-        return jsonify(message='Willkommen ' + name)
-
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String, unique=True)
-    password = Column(String)
-
-
-class Planet(db.Model):
-    __tablename__ = 'planets'
-    planet_id = Column(Integer, primary_key=True)
-    planet_name = Column(String)
-    planet_type = Column(String)
-    home_star = Column(String)
-    mass = Column(Float)
-    radius = Column(Float)
-    distance = Column(Float)
+ma = Marshmallow(app)
 
 
 @app.cli.command('db_create')
@@ -105,6 +58,78 @@ def db_seed():
 
     db.session.commit()
     print('Database seeded')
+
+
+@app.route('/')
+def hello_world():
+    return jsonify(message='Hallo Welt!!'), 200
+
+
+@app.route('/not_found')
+def not_found():
+    return jsonify(message='not found'), 404
+
+
+@app.route("/parameters")
+def parameters():
+    name = request.args.get('name')
+    age = int(request.args.get('age'))
+    if age < 18:
+        return jsonify(message='leider ' + name), 401
+    else:
+        return jsonify(message='Willkommen ' + name)
+
+
+@app.route('/url_vars/<string:name>/<int:age>')
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message='leider ' + name), 401
+    else:
+        return jsonify(message='Willkommen ' + name)
+
+
+@app.route('/planets', methods=['GET'])
+def planets():
+    planets_list = Planet.query.all()
+    result = planets_schema.dump(planets_list)
+    return jsonify(result)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
+
+
+class Planet(db.Model):
+    __tablename__ = 'planets'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String)
+    planet_type = Column(String)
+    home_star = Column(String)
+    mass = Column(Float)
+    radius = Column(Float)
+    distance = Column(Float)
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
+
+
+class PlanetSchema(ma.Schema):
+    class Meta:
+        fields = ('planet_id', 'planet_name', 'planet_type', 'home_star', 'mass', 'radius', 'distance')
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+planet_schema = PlanetSchema()
+planets_schema = PlanetSchema(many=True)
 
 
 if __name__ == '__main__':
